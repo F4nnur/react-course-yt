23ae6c0a3b4531e14bb40ec158eb9cd8dcf58955
Fannur <Fann1kF@yandex.ru>
2023-02-02 17:51:03 +0300

    +21. dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import s from './Dialogs.module.scss';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.Dialogs}>
+            <div className={s.DialogsItems}>
+                <div className={s.Dialog}>
+                    Andrey
+                </div>
+                <div className={s.Dialog}>
+                    Dima
+                </div>
+                <div className={s.Dialog}>
+                    Olga
+                </div>
+            </div>
+            <div className={s.Messages}>
+                <div className={s.Message}>Hi</div>
+                <div className={s.Message}>Hello</div>
+                <div className={s.Message}>Kek</div>
+            </div>
         </div>
     );
 };
