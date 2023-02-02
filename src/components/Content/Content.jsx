3137f2e0e37db77e5e63df789f739c7a29a72979
Fannur <Fann1kF@yandex.ru>
2023-02-03 00:34:31 +0300

    +24. BLL - Ui data

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,11 +3,18 @@ import Post from "./Posts/Post/Post";
 import ProfileInfo from "./ProfileInfo";
 
 const Content = () => {
+
+    let postData = [
+        {id: 1, data: 'Hi'},
+        {id: 2, data: 'Hello'},
+        {id: 3, data: 'Cool'},
+    ]
     return (
         <div>
             <ProfileInfo/>
-            <Post message={"Hi"}/>
-            <Post message={"Hello"}/>
+            <Post message={postData[0].data}/>
+            <Post message={postData[1].data}/>
+            <Post message={postData[2].data}/>
         </div>
     );
 };
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,17 +4,27 @@ import DialogItem from "./DialogItem";
 import Message from "./Message";
 
 const Dialogs = () => {
+    let dialogData = [
+        {id: 1, name: 'Dima'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Olya'},
+    ];
+
+    let messageData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'kek'},
+        {id: 3, message: 'lol'},
+    ]
     return (
         <div className={s.Dialogs}>
             <div className={s.DialogsItems}>
-                <DialogItem name={'Dima'} id={'1'}/>
-                <DialogItem name={'Andrey'} id={'2'}/>
-                <DialogItem name={'Olya'} id={'3'}/>
+                <DialogItem name={dialogData[0].name} id={dialogData[0].id}/>
+                <DialogItem name={dialogData[1].name} id={dialogData[1].id}/>
             </div>
             <div className={s.Messages}>
-                <Message message={'Hi'}/>
-                <Message message={'Kek'}/>
-                <Message message={'Lol'}/>
+                <Message message={messageData[0].message}/>
+                <Message message={messageData[1].message}/>
+                <Message message={messageData[2].message}/>
             </div>
         </div>
     );
